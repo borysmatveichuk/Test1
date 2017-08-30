9ebf46146e2ed1d29f9b752e1b0ea35ff672bc39
unknown <boris.matveychuck@gmail.com>
2017-08-30 08:59:15 +0300

    - create annotations

---
+++ app/src/main/java/com/example/borysm/test1/di/PerActivity.java
@@ -0,0 +1,11 @@
+package com.example.borysm.test1.di;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PerActivity {
+}
